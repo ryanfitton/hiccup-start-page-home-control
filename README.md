edb40a1690e0550b450156fa5e83cbdb71b2e87d
Ryan Fitton <ryan@ryanfitton.co.uk>
2024-12-28 20:33:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hiccup Start Page
+## Home Control Tablet
+
+Original repo URL: [https://github.com/ashwin-pc/hiccup](https://github.com/ashwin-pc/hiccup)
+
+### How to use
+
+* All settings are defined in `config.json`
+
+* Custom images have been created, a GIMP file has been saved in `assets`.
+
+* Place onto a web server, set your web browser start page to the webserver's IP address
+
+### Modifications
+
+* Within `static/css/main.e48286b8.chunk.css` one line of CSS has been added to hide the search bar.
---
+++ precache-manifest.53b2fec7028c3be6a4e3cbf318e87c75.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9aabf34b383ed9595ef1b050c6e920",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2047a23253f3adcee361",
+    "url": "./static/css/main.e48286b8.chunk.css"
+  },
+  {
+    "revision": "2ad62213aadf7d2c4849",
+    "url": "./static/js/2.389bece9.chunk.js"
+  },
+  {
+    "revision": "a6d5bab58f82b51182f890af1f1a3df8",
+    "url": "./static/js/2.389bece9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2047a23253f3adcee361",
+    "url": "./static/js/main.59c8f530.chunk.js"
+  },
+  {
+    "revision": "ffd5ac60f9679bede73d",
+    "url": "./static/js/runtime-main.c6b70e5b.js"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "./static/media/cog-solid.9f07a3df.svg"
+  },
+  {
+    "revision": "43e8e81ad0d1cd709f186ba678835a01",
+    "url": "./static/media/folder-plus-solid.43e8e81a.svg"
+  },
+  {
+    "revision": "f0bbb0d3fd34e2f766026b045a5fddd4",
+    "url": "./static/media/globe-americas-solid.f0bbb0d3.svg"
+  },
+  {
+    "revision": "912bdd59ec1d6e67ada14a827fbbebd8",
+    "url": "./static/media/pen-solid.912bdd59.svg"
+  },
+  {
+    "revision": "2de4ac5c059bd49b4f32d7047e06f4d4",
+    "url": "./static/media/plus-circle-solid.2de4ac5c.svg"
+  },
+  {
+    "revision": "80d694282325ef06e18cbd3572a2ddec",
+    "url": "./static/media/save-solid.80d69428.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "./static/media/search-solid.c6a0effb.svg"
+  },
+  {
+    "revision": "184843881820ca3e3422f3a1c43a4f04",
+    "url": "./static/media/star-regular.18484388.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "./static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "./static/media/trash-solid.a3e4c2f7.svg"
+  },
+  {
+    "revision": "7016a569d101b2c112d99c0679443eea",
+    "url": "./static/media/undo-solid.7016a569.svg"
+  }
+]);
